b535c22955f709086de82ebe550e0fc5e8b7959b
VictorStankov <viktorstankov1@gmail.com>
2023-03-12 12:11:31 +0200

    feat: Create endpoint for directly joining a room

--- app.py
+++ app.py
@@ -104,8 +104,28 @@ def room():
     return render_template('room.html', room_name=room_name, cards=rooms[room_code]['cards'], room_code=room_code)
 
 
+@app.route('/join_room', methods=['GET', 'POST'])
+def join_room_extern():
+    if request.method == 'GET':
+        room_code = request.args.get('room')
+        if not isinstance(room_code, str) or room_code not in rooms:
+            return redirect(url_for('home'))
+        return render_template('join.html', room_code=room_code)
+    else:
+        room_code = request.form.get('room_code')
+        display_name = request.form.get('display_name')
 
+        if room_code not in rooms.keys():
+            print(rooms, room_code)
+            session['error'] = 'Room not found!'
+            return redirect(url_for('home'))
 
+        if not isinstance(display_name, str) or len(display_name) < 5:
+            return render_template('join.html', error='Name too short!', room_code=room_code)
+        session['room_code'] = room_code
+        session['display_name'] = display_name
+
+        return redirect(url_for('room'))
 
 
 @socketio.on('connect')
