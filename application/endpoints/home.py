8f3f94a2f9aa2800675d4dfbb553fb922523db2a
VictorStankov <viktorstankov1@gmail.com>
2023-03-14 12:19:50 +0200

    fix: Styling and better error handling

--- application/endpoints/home.py
+++ application/endpoints/home.py
@@ -1,6 +1,8 @@
 import random
 from string import ascii_uppercase
+
 from flask import request, render_template, session, url_for, redirect
+
 from application import app, rooms
 
 card_def = {
@@ -33,35 +35,7 @@ def home():
     create_btn = request.form.get('create', False)
     join_btn = request.form.get('join', False)
 
-    if join_btn is not False:
-        display_name = request.form.get('display_name')
-        room_code = request.form.get('room_code')
-
-        if display_name == '':
-            return render_template(
-                'home.html',
-                error='Please enter a name',
-                display_name=display_name, room_code=room_code
-            )
-
-        if room_code == '':
-            return render_template(
-                'home.html',
-                error='Please enter a room code',
-                display_name=display_name, room_code=room_code
-            )
-
-        if room_code not in rooms:
-            return render_template(
-                'home.html',
-                error='Room does not exist',
-                display_name=display_name, room_code=room_code
-            )
-
-        session['display_name'] = display_name
-        session['room_code'] = room_code
-
-    elif create_btn is not False:
+    if create_btn is not False:
         room_name = request.form.get('room_name')
         cards = request.form.get('cards')
 
@@ -86,6 +60,40 @@ def home():
         session['room_name'] = room_name
         session['room_code'] = room_code
         session['display_name'] = 'Host'
+
+    elif join_btn is not False:
+        display_name = request.form.get('display_name')
+        room_code = request.form.get('room_code')
+
+        if not isinstance(display_name, str) or display_name == '':
+            return render_template(
+                'home.html',
+                error='Please enter a name',
+                room_code=room_code
+            )
+        elif len(display_name) < 5:
+            return render_template(
+                'home.html',
+                error='Name too short',
+                room_code=room_code
+            )
+
+        if not isinstance(room_code, str) or room_code == '':
+            return render_template(
+                'home.html',
+                error='Please enter a room code',
+                display_name=display_name
+            )
+
+        if room_code not in rooms:
+            return render_template(
+                'home.html',
+                error='Room does not exist',
+                display_name=display_name
+            )
+
+        session['display_name'] = display_name
+        session['room_code'] = room_code
     else:
         return render_template('home.html')
 
