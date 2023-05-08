5d690d83a9c395ec70fb9fdb6eda224fa8be4c78
VictorStankov <viktorstankov1@gmail.com>
2023-05-08 17:17:25 +0300

    chore: Encapsulate Room endpoints in class

--- application/endpoints/session_room.py
+++ application/endpoints/session_room.py
@@ -3,21 +3,23 @@ from flask import request, render_template, session, url_for, redirect
 from application import app, ScrumPoker
 
 
-@app.get('/room')
-def get_room():
-    room_code = session.get('room_code')
-    display_name = session.get('display_name')
-    if room_code is None or display_name is None or not ScrumPoker.room_exists(room_code):
-        return redirect(url_for('home_get'))
+class RoomEndpoints:
+    @staticmethod
+    @app.get('/room')
+    def get_room():
+        room_code = session.get('room_code')
+        display_name = session.get('display_name')
+        if room_code is None or display_name is None or not ScrumPoker.room_exists(room_code):
+            return redirect(url_for('home_get'))
 
-    room = ScrumPoker.get_room(room_code)
+        room = ScrumPoker.get_room(room_code)
 
-    return render_template(
-        'room.html',
-        room_name=room.room_name,
-        cards=room.cards,
-        room_code=room.room_code
-    )
+        return render_template(
+            'room.html',
+            room_name=room.room_name,
+            cards=room.cards,
+            room_code=room.room_code
+        )
 
 
 @app.get('/join_room')
